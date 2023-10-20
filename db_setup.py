ccd4ed3dcc52786d2b0ddaa6f13af3d6297e3cd7
TobyJWalker <tobyjwalker03@outlook.com>
2023-10-20 23:55:18 +0100

    added file to modify databases manually if necessary

---
+++ db_setup.py
@@ -0,0 +1,9 @@
+from seeds.seed_db import *
+
+# call the seed_test_data function
+def setup_test_data():
+    seed_test_data()
+
+# call the create_tables function
+def setup_tables():
+    create_tables()
