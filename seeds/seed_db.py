06709a1d3f96d0ed0cb6b4185a99c0ef36e31157
TobyJWalker <tobyjwalker03@outlook.com>
2023-10-20 23:50:37 +0100

    Added create, reset and seed function

---
+++ seeds/seed_db.py
@@ -0,0 +1,31 @@
+from lib.models import *
+
+# force reset tables to empty state
+def _reset_tables():
+    db.drop_tables([User])
+    db.create_tables([User])
+
+# create empty tables if they don't exist
+def create_tables():
+    db.create_tables([User])
+
+# seed the database with test data
+def seed_test_data():
+    _reset_tables()
+
+    # create test users
+    User.create(
+        username="test_1", 
+        password="test_1_password", 
+        email="test_1@example.com"
+        )
+    User.create(
+        username="test_2", 
+        password="test_2_password", 
+        email="test_2@example.com"
+    )
+    User.create(
+        username="test_3",
+        password="test_3_password",
+        email="test_3@example.com"
+    )
