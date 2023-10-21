6fdb9ced25cd52a06d6db125714a57cae8b52575
TobyJWalker <tobyjwalker03@outlook.com>
2023-10-21 14:08:43 +0100

    Removed unnecessary use of environment variables for test database

--- lib/models.py
+++ lib/models.py
@@ -4,17 +4,12 @@ from datetime import datetime
 APP_ENV = os,os.environ.get("APP_ENV")
 
 if APP_ENV == "test":
-    DB_NAME = os.environ.get("TEST_DB_NAME")
-    DB_USER = os.environ.get("TEST_DB_USER")
-    DB_PASSWORD = os.environ.get("TEST_DB_PASSWORD")
-
     db = peewee.PostgresqlDatabase(
-        DB_NAME, user=DB_USER, password=DB_PASSWORD, host='localhost', port=5432
+        'test-todo-db', user='tester', password='testing', host='localhost', port=5432
     )
 
 else:
-    db = peewee.PostgresqlDatabase('todo-db')
-
+    db = peewee.PostgresqlDatabase(os.environ.get('POSTGRES_DB'))
 
 class User(peewee.Model):
     username = peewee.CharField(unique=True)
